11baf835db164ae2ee5a593abb9306281ebce8e0
tonya <tonya@tokia.dev>
2023-05-18 21:58:00 +0000

    feat: make speedy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
-
 import react from "@astrojs/react";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image({
     serviceEntryPoint: '@astrojs/image/sharp'
-  }), react()]
+  }), react(), prefetch()]
 });
