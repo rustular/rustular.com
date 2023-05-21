04ea8ee550e0b21f7cd4ea08452595cc1e5bd2f1
tonya <tonya@tokia.dev>
2023-05-21 17:36:01 +0000

    feat: add basic email box

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
 import react from '@astrojs/react';
-
 import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
