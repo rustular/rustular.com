11cd1dc3db9aa23a025d4df9e242635a7ca018e0
tonya <tonya@tokia.dev>
2023-05-23 20:55:56 +0000

    fix: url

--- src/api.ts
+++ src/api.ts
@@ -1,12 +1,12 @@
 import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import type { AppRouter } from './pages/trpc';
+import type { AppRouter } from './pages/trpc/[...trpc]';
 
 const client = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
       url: import.meta.env.DEV
         ? 'http://localhost:3000/trpc'
-        : import.meta.env.CF_PAGES_URL,
+        : `${import.meta.env.CF_PAGES_URL}/trpc`,
     }),
   ],
 });
