55b0e958b1fead8be2665a7ee29db2346b488247
tonya <tonya@tokia.dev>
2023-05-26 14:07:11 +0000

    fix: stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import prefetch from '@astrojs/prefetch';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rustular.pages.dev',
   integrations: [tailwind(), react(), prefetch()],
-  output: 'hybrid',
-  experimental: {
-    hybridOutput: true,
-  },
-  adapter: cloudflare(),
 });
